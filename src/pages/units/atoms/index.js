7b9abae50170b75b6b6b2e908724f36de5ebad83
mikedevv <maungdevv@gmail.com>
2025-01-30 14:08:32 +0700

    feat: autocomplete for states, district and townships

--- src/pages/units/atoms/index.js
+++ src/pages/units/atoms/index.js
@@ -1,4 +1,71 @@
-import { atom } from "jotai"
+import { atom } from "jotai";
 
-export const statusFilterAtom = atom()
-export const fetchedPositionsAtom = atom([])
+import data from '../../../assets/geo.json'
+
+export const statusFilterAtom = atom();
+export const fetchedPositionsAtom = atom([]);
+
+export const selectedCreateStateKeyAtom = atom({});
+export const selectedCreateDistrictKeyAtom = atom({});
+export const selectedCreatedTownshipKeyAtom = atom({});
+const states = data.map((d) => ({ key: d.mm, label: d.mm }));
+
+export const stateAtom = atom(states);
+export const districtAtom = atom((get) => filterDistricts(get(selectedCreateStateKeyAtom)));
+export const townshipAtom = atom((get) => filterTownships(get(selectedCreateStateKeyAtom), get(selectedCreateDistrictKeyAtom)));
+
+
+const filterDistricts = (state) => {
+  const filterData = data.filter((d) => d.mm === state.state)[0];
+  const refinedData = filterData?.districts.map((d) => {
+    return { key: d.mm, label: d.mm };
+  });
+  return refinedData;
+};
+
+const filterTownships = (state, district) => {
+  if (Object.keys(state).length === 0 || Object.keys(district).length === 0)
+    return;
+  const filterData = data.filter((d) => d.mm === state?.state)[0];
+  const filterTownshipData = filterData?.districts.filter(
+    (d) => d.mm === district?.district
+  )[0];
+  return filterTownshipData?.townships.map((d) => {
+    return { key: d.mm, label: d.mm };
+  });
+};
+export const levelDetailsAtom = atom(
+    (get) => {
+      const selectedState = get(selectedCreateStateKeyAtom);
+      const selectedDistrict = get(selectedCreateDistrictKeyAtom);
+      const selectedTownship = get(selectedCreatedTownshipKeyAtom);
+  
+      // Build level_details dynamically
+      const levelDetails = {};
+  
+      if (selectedState) {
+        levelDetails.state = { name: selectedState.state };
+      }
+  
+      if (selectedDistrict) {
+        levelDetails.district = { name: selectedDistrict.district };
+      }
+  
+      if (selectedTownship) {
+        levelDetails.township = { name: selectedTownship.township };
+      }
+  
+      return levelDetails;
+    },
+    (get, set, update) => {
+      if (update.state) {
+        set(selectedCreateStateKeyAtom, { state: update.state });
+      }
+      if (update.district) {
+        set(selectedCreateDistrictKeyAtom, { district: update.district });
+      }
+      if (update.township) {
+        set(selectedCreatedTownshipKeyAtom, { township: update.township });
+      }
+    }
+  );
--- src/pages/units/table-components/CreateModal.jsx
+++ src/pages/units/table-components/CreateModal.jsx
@@ -1,18 +1,9 @@
-import React from 'react'
-import {
-    Modal,
-    ModalContent,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
-    Button,
-    Form,
-    Input,
-    Autocomplete,
-    AutocompleteItem,
-} from "@heroui/react";
+import { Autocomplete, AutocompleteItem, Button, Form, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from "@heroui/react";
 import { useAtom } from 'jotai';
-import { fetchedPositionsAtom } from '../atoms';
+import React from 'react'
+
+import { districtAtom, fetchedPositionsAtom, levelDetailsAtom, selectedCreateDistrictKeyAtom, selectedCreatedTownshipKeyAtom, selectedCreateStateKeyAtom, stateAtom, townshipAtom } from '../atoms';
+
 // const positions = [
 //     { key: "1", label: "Software Engineer" },
 //     { key: "2", label: "Product Manager" },
@@ -26,7 +17,36 @@ const status = [
 ]
 const CreateModal = ({ isOpen, onClose }) => {
     const [fetchedPositions,] = useAtom(fetchedPositionsAtom)
-    const {positions} = fetchedPositions;
+    const { positions } = fetchedPositions;
+    const [states,] = useAtom(stateAtom)
+    const [disctricts,] = useAtom(districtAtom)
+    const [townships,] = useAtom(townshipAtom)
+    const [selectedStateKey, setSelectedStateKey] = useAtom(selectedCreateStateKeyAtom);
+    const [selectedDistrictKey, setSelectedDistrictKey] = useAtom(selectedCreateDistrictKeyAtom);
+    const [selectedTownshipKey, setSelectedTownshipKey] = useAtom(selectedCreatedTownshipKeyAtom);
+    const [levelDetails, setLevelDetails] = useAtom(levelDetailsAtom)
+    const onStateSelectionChange = (state) => {
+        if (state === null) {
+            setSelectedStateKey({ state: null, level: null });
+            setSelectedDistrictKey({ district: null, level: null });
+            setSelectedTownshipKey({ township: null, level: null });
+        } else {
+            setSelectedStateKey({ state, level: "state" });
+            setLevelDetails({ state: state.name });
+        }
+    };
+
+    const onDistrictSelectionChange = (district) => {
+        setSelectedDistrictKey(district ? { district, level: "district" } : { district: null, level: null });
+        setLevelDetails({ district: district.name });
+    };
+
+    const onTownshipSelectionChange = (township) => {
+        setSelectedTownshipKey(township ? { township, level: "township" } : { township: null, level: null });
+        setLevelDetails({ township: township.name });
+    };
+    console.log('districts', disctricts)
+    console.log('townships', townships)
     return (
         <div>
             <Modal radius='none' backdrop={"blur"} isOpen={isOpen} onClose={onClose}>
@@ -38,6 +58,66 @@ const CreateModal = ({ isOpen, onClose }) => {
                                 <Form>
                                     <Input type="text" placeholder="နာမည်ရိုက်ပါ" radius='none' />
                                     <Input type="text" placeholder="လိပ်စာရိုက်ပါ" radius='none' />
+                                    <Autocomplete
+                                        className="max-w-xs rounded-none"
+                                        defaultItems={states}
+                                        onSelectionChange={onStateSelectionChange}
+                                        classNames={{
+                                            listboxWrapper: "rounded-none",
+                                            listbox: "rounded-none",
+                                            base: "rounded-none",
+                                            popoverContent: "rounded-none",
+                                        }}
+                                        label="ပြည်နယ်"
+                                        placeholder="ပြည်နယ်ရွေးချယ်ပါ"
+                                        radius='none'
+                                    >
+                                        {(state) => <AutocompleteItem classNames={{
+                                            base: 'rounded-none',
+                                            selected: 'bg-blue-500',
+                                            list: "rounded-none"
+                                        }} key={state.key}>{state.label}</AutocompleteItem>}
+                                    </Autocomplete>
+                                    {(disctricts && selectedStateKey.state) && <Autocomplete
+                                        onSelectionChange={onDistrictSelectionChange}
+                                        className="max-w-xs rounded-none"
+                                        defaultItems={disctricts}
+                                        classNames={{
+                                            listboxWrapper: "rounded-none",
+                                            listbox: "rounded-none",
+                                            base: "rounded-none",
+                                            popoverContent: "rounded-none",
+                                        }}
+                                        label="ခရိုင်"
+                                        placeholder="ခရိုင်ရွေးချယ်ပါ"
+                                        radius='none'
+                                    >
+                                        {(disctrict) => <AutocompleteItem classNames={{
+                                            base: 'rounded-none',
+                                            selected: 'bg-blue-500',
+                                            list: "rounded-none"
+                                        }} key={disctrict.key}>{disctrict.label}</AutocompleteItem>}
+                                    </Autocomplete>}
+                                    {(townships && selectedDistrictKey.district) && <Autocomplete
+                                        className="max-w-xs rounded-none"
+                                        defaultItems={townships}
+                                        onSelectionChange={onTownshipSelectionChange}
+                                        classNames={{
+                                            listboxWrapper: "rounded-none",
+                                            listbox: "rounded-none",
+                                            base: "rounded-none",
+                                            popoverContent: "rounded-none",
+                                        }}
+                                        label="ခရိုင်"
+                                        placeholder="ခရိုင်ရွေးချယ်ပါ"
+                                        radius='none'
+                                    >
+                                        {(township) => <AutocompleteItem classNames={{
+                                            base: 'rounded-none',
+                                            selected: 'bg-blue-500',
+                                            list: "rounded-none"
+                                        }} key={township.key}>{township.label}</AutocompleteItem>}
+                                    </Autocomplete>}
                                     <Autocomplete
                                         className="max-w-xs rounded-none"
                                         defaultItems={positions}
@@ -56,25 +136,6 @@ const CreateModal = ({ isOpen, onClose }) => {
                                             selected: 'bg-blue-500',
                                             list: "rounded-none"
                                         }} key={animal._id}>{animal.name}</AutocompleteItem>}
-                                    </Autocomplete>
-                                    <Autocomplete
-                                        className="max-w-xs rounded-none"
-                                        defaultItems={status}
-                                        classNames={{
-                                            listboxWrapper: "rounded-none",
-                                            listbox: "rounded-none",
-                                            base: "rounded-none",
-                                            popoverContent: "rounded-none",
-                                        }}
-                                        label="status"
-                                        placeholder="choose one status"
-                                        radius='none'
-                                    >
-                                        {(animal) => <AutocompleteItem classNames={{
-                                            base: 'rounded-none',
-                                            selected: 'bg-blue-500',
-                                            list: "rounded-none"
-                                        }} key={animal.key}>{animal.label}</AutocompleteItem>}
                                     </Autocomplete>
                                     <Autocomplete
                                         className="max-w-xs rounded-none"
