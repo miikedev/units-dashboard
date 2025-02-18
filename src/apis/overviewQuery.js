b3e14dcc875a824684c1a602b7fa715f74855cef
mikedevv <maungdevv@gmail.com>
2025-02-19 02:08:26 +0700

    feat: over view without N table

--- src/apis/overviewQuery.js
+++ src/apis/overviewQuery.js
@@ -3,7 +3,7 @@ import { fetchOverview } from "./overview";
 export const useOverviewQuery = () => {  
     return useQuery(  
         {  
-            queryKey: ['units', 'counts'], // Include currentPage in the queryKey  
+            queryKey: ['overview'], // Include currentPage in the queryKey  
             queryFn: () => fetchOverview(),  
         }  
     );  
--- src/pages/overview/overview.jsx
+++ src/pages/overview/overview.jsx
@@ -3,7 +3,7 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 import { Button } from "@heroui/button";
 import { useAtom } from "jotai";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import PieChart from "./pie-chart";
 
@@ -14,60 +14,81 @@ import { useOverviewQuery } from "@/apis/overviewQuery";
 import Error from "@/components/Error";
 import { LoaderIcon } from "lucide-react";
 
-
 const Overview = () => {
-  const { data, isSuccess, isPending, isError, error } = useOverviewQuery();
-  const [overview, setOverview] = useAtom(overviewAtom);
+  const { data, isSuccess, isPending, isError } = useOverviewQuery();
+  const [statusSum, setStatusSum] = useState({ A: 0, N: 0, C: 0 });
   const [showStatus, setShowStatus] = useAtom(showStatusAtom);
-
   useEffect(() => {
-    if (data && data !== overview) {
-      setOverview(data);
+    if (isSuccess && data?.data) {
+      const initialSum = { A: 0, N: 0, C: 0 };
+
+      const calculatedSum = data.data.reduce((acc, item) => ({
+        A: acc.A + (item.count?.A ?? 0),
+        N: acc.N + (item.count?.N ?? 0),
+        C: acc.C + (item.count?.C ?? 0)
+      }), initialSum);
+
+      setStatusSum(calculatedSum);
     }
-  }, [data, overview, setOverview]);
-
-  const {
-    totalUnits = 0,
-    activeUnits = 0,
-    inactiveUnits = 0,
-    statusZeroPositionsCount = 0,
-    statusZeroPositions = [],
-    dateRange = "",
-    unitsByLevel = [],
-    activeUnitsByLevel = [],
-    inactiveUnitsByLevel = [],
-  } = overview || {};
-
+  }, [data, isSuccess]);
+  console.log('data', data?.data)
+  console.log('Overview status sum:', statusSum);
+  const result = data?.data.flatMap(item =>
+    Object.entries(item.count).map(([status, count]) => ({
+      level: item.level,
+      status,
+      count,
+      fill: item.fill
+    }))
+  );
+  console.log('result', result)
+  const filterAStatus = result.filter(i => i.status === 'A')
+  const filterCStatus = result.filter(i => i.status === 'C')
+  const filterNStatus = result.filter(i => i.status === 'N')
+  const pieData = [
+    {
+      name: 'A',
+      value: filterAStatus
+    },
+    {
+      name: 'C',
+      value: filterCStatus
+    },
+    {
+      name: 'N',
+      value: filterNStatus
+    }
+  ]
+  console.log('pieData', pieData)
   if (isPending) return <div className="flex justify-center items-center w-full h-3/4"><Loading /></div>;
   if (isError) return <div className="flex justify-center items-center w-full h-3/4"><Error /></div>
-
-  if (!isSuccess || !overview) return null;
+  if (!isSuccess || !statusSum) return null;
 
   const pieChartData = [
-    { data: unitsByLevel, label: "Total Units" },
-    { data: activeUnitsByLevel, label: "Active Units" },
-    { data: inactiveUnitsByLevel, label: "Inactive Units" },
+    { data: statusSum?.N + statusSum?.A + statusSum?.C, label: "Total Units" },
+    { data: statusSum?.A, label: "Active Units" },
+    { data: statusSum?.C, label: "Inactive Units" },
   ];
-
+  console.log('pieChartData', pieChartData)
   return (
     <div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 my-5">
         <Card>
           <CardHeader>ခန့်ထားပြီးသူ နှင့် လျာထားပြီးသူ စုစုပေါင်း</CardHeader>
-          <CardContent>{totalUnits}</CardContent>
+          <CardContent>{statusSum.A + statusSum.C}</CardContent>
         </Card>
         <Card>
           <CardHeader>ခန့်ထားပြီးသူ စုစုပေါင်း</CardHeader>
-          <CardContent>{activeUnits}</CardContent>
+          <CardContent>{statusSum.A}</CardContent>
         </Card>
         <Card>
           <CardHeader>လျာထားပြီးသူ စုစုပေါင်း</CardHeader>
-          <CardContent>{inactiveUnits}</CardContent>
+          <CardContent>{statusSum.C}</CardContent>
         </Card>
         <Card>
           <CardHeader>လစ်လပ်ရာထူးများ</CardHeader>
           <CardContent className="flex justify-between items-end">
-            <span>{statusZeroPositionsCount}</span>
+            <span>{statusSum.N}</span>
             <Button
               variant={showStatus ? "outline" : "primary"}
               className="font-thin relative top-[.5rem]"
@@ -80,14 +101,18 @@ const Overview = () => {
       </div>
       {showStatus && (
         <div className="mb-5">
-          <InactivePositionTable data={statusZeroPositions} />
+          {/* <InactivePositionTable data={statusZeroPositions} /> */}
         </div>
       )}
       <Card>
+        <CardHeader className="font-bold text-center">Overview Pie Charts of Positions</CardHeader>
         <div className="flex w-full justify-between">
-          {pieChartData.map(({ data, label }, index) => (
-            <PieChart key={index} data={data} dateRange={dateRange} label={label} />
-          ))}
+          {pieData.map(({value, name}, index) => {
+            console.log(data)
+            return (
+              <PieChart key={index} data={value} label={name} />
+            )
+          })}
         </div>
       </Card>
     </div>
